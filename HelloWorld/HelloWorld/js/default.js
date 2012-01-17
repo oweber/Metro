cb82e2cbe3b0e483fb11a278a32a6694bf65cdbf
Oliver Weber <nexiles@win8vm.(none)>
2012-01-17 17:44:26 +0100

    my very first metro app

---
+++ HelloWorld/HelloWorld/js/default.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+    // Uncomment the following line to enable first chance exceptions.
+    // Debug.enableFirstChanceException(true);
+
+    WinJS.Application.onmainwindowactivated = function (e) {
+        if (e.detail.kind === Windows.ApplicationModel.Activation.ActivationKind.launch) {
+            var counter = 0;
+            document.addEventListener("click", function () {
+                counter++;
+                text.innerText = "Hello Nexiles! You clicked " + counter + " times...";
+            });
+        }
+    }
+
+    WinJS.Application.start();
+})();
